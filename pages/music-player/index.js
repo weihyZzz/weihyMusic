111d78a0a7ef28caac0f5ca5b18187173633076f
weihy <971973918@qq.com>
2023-06-27 17:03:31 +0800

    播放页面初始化

--- pages/music-player/index.js
+++ pages/music-player/index.js
@@ -7,7 +7,9 @@ Page({
      */
     data: {
       id: 0,
-      currentSong: {}
+      currentSong: {},
+      currentPage: 0,
+      contentHeight: 0
     },
 
     /**
@@ -19,6 +21,13 @@ Page({
       this.setData({id: id})
         // 2.根据id获取歌曲详情信息
       this.getPageData(id)
+       //  3.动态计算内容页高度
+       const globalData = getApp().globalData
+       const screenHeight = globalData.screenHeight
+       const statusBarHeight = globalData.statusBarHeight
+       const navBarHeight = globalData.navBarHeight
+       const contentHeight = screenHeight - statusBarHeight - navBarHeight
+       this.setData({contentHeight})
     },
     getPageData: function(id) {
         getSongDetail(id).then(res => {
@@ -26,5 +35,10 @@ Page({
             console.log(res);
             this.setData({ currentSong: res.songs[0] })
         })
+    },
+    // 事件处理
+    handleSwiperChange: function(event) {
+        const current = event.detail.current
+        this.setData({currentPage: current})
     }
 })
