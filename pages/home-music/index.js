c7a8912afdbcdc207efa8a1e61540fc4ae09fb18
weihy <971973918@qq.com>
2023-07-06 16:55:40 +0800

    新增无播放歌曲，隐藏底部播放栏功能

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -26,7 +26,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function() {
-        playerStore.dispatch("playMusicWithSongIdAction", { id: 1974443814 })
+        // playerStore.dispatch("playMusicWithSongIdAction", { id: 1974443814 })
 
         this.getPageData()
         
